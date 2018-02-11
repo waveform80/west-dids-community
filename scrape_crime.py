64f8ed77f521c814348f07dcc3cf124afd4956e1
Dave Jones <dave@waveform.org.uk>
2018-02-11 17:56:56 +0000

    Use police.uk as the data source

--- scrape_crime.py
+++ scrape_crime.py
@@ -2,89 +2,91 @@ import io
 import json
 import datetime as dt
 from operator import itemgetter
+from time import sleep
+from itertools import groupby
 
-import bs4
-import requests
+from dateutil.relativedelta import relativedelta
+from police_api import PoliceAPI
 import sqlalchemy as sa
 
-URL = "http://www.ukcrimestats.com/Neighbourhood/Greater_Manchester_Police/Didsbury_West"
+api = PoliceAPI()
+force = api.get_force('greater-manchester')
+location = api.get_neighbourhood(force, 'EC25')
 
-page = requests.get(URL)
-soup = bs4.BeautifulSoup(page.text, 'html5lib')
-table = soup.find('table') # it's the first <table>
-
-def parse_cells(row):
-    for ix, cell in enumerate(row):
-        if ix == 0:
-            yield dt.datetime.strptime(cell.text, '%b %Y').date()
-        elif 'colspan' in cell.attrs:
-            # Ignore spanned values; these are historically "combined" values
-            # which are now separated in the stats. To avoid drawing invaild
-            # inferences we simply ignore the historic combined ones
-            for span in range(int(cell.attrs['colspan'])):
-                yield None
-        else:
-            yield int(cell.text)
-
-replacements = {
-    'ASB': 'Anti-social Behaviour',
-    'CD&A': 'Criminal Damage & Arson',
+categories = {
+    category.id: category.name
+    for category in api.get_crime_categories()
 }
 
-header = [
-    replacements.get(th.text, th.text)
-    for tr in table.find('thead').find_all('tr')
-    for th in tr.find_all('th')[1:]
-]
+def months(start=dt.date(2015, 1, 1)):
+    month = start
+    latest = dt.datetime.strptime(api.get_latest_date(), '%Y-%m').date()
+    while month <= latest:
+        yield month
+        month += relativedelta(months=1)
 
-data = [
-    [cell for cell in parse_cells(tr)]
-    for tr in table.find('tbody').find_all('tr')
-]
-data = sorted(
-    data,
-    key=itemgetter(0)
-)
+data = {
+    month: {
+        category: len(list(crimes))
+        for category, crimes in
+        groupby(
+            sorted(
+                api.get_crimes_area(location.boundary, '{0:%Y-%m}'.format(month)),
+                key=lambda crime: crime.category.id),
+            key=lambda crime: crime.category.id)
+    }
+    for month in months()
+}
+
+data = {
+    category: {
+        month: data[month][category]
+        for month in months()
+        if category in data[month]
+    }
+    for category in categories
+}
 
 engine = sa.create_engine('sqlite:///crime.db')
 metadata = sa.MetaData()
 table = sa.Table(
     'crime_stats', metadata,
-    sa.Column('month', sa.Date, primary_key=True),
-    sa.Column('crime', sa.String(max(len(s) for s in header)), primary_key=True),
+    sa.Column('month', sa.Date, nullable=False),
+    sa.Column('category', sa.String(100), nullable=False),
     sa.Column('incidents', sa.Integer, nullable=False),
+    sa.PrimaryKeyConstraint('month', 'category', name='pk'),
     sa.CheckConstraint('incidents >= 0', name='incidents_ck')
 )
-metadata.drop_all(engine)
 metadata.create_all(engine)
 
 conn = engine.connect()
 insert = table.insert()
-with conn.begin():
-    for row in data:
-        month, *month_incidents = row
-        for crime, incidents in zip(header, month_incidents):
-            if incidents is not None:
-                conn.execute(insert, month=month, crime=crime, incidents=incidents)
-
-with conn.begin():
-    with io.open('crime.json', 'w', encoding='utf-8') as f:
-        f.write(json.dumps([
-            {
-                'crime': outer.crime,
-                'incidents': [
-                    {
-                        'month': inner.month.strftime('%Y-%m-%d'),
-                        'count': inner.incidents,
-                    }
-                    for inner in
-                    conn.execute(sa.select([table.c.month, table.c.incidents]).
-                                 where(table.c.crime == outer.crime).
-                                 order_by(table.c.month))
-                ],
-            }
-            for outer in conn.execute(sa.select([table.c.crime], distinct=True).
-                                      order_by(
-                                          sa.func.coalesce(
-                                              sa.func.nullif(table.c.crime, 'Total'), 'ZZZ')))
-        ]))
+update = table.update()
+#with conn.begin():
+#    for row in data:
+#        month, *month_incidents = row
+#        for crime, incidents in zip(header, month_incidents):
+#            if incidents is not None:
+#                conn.execute(insert, month=month, crime=crime, incidents=incidents)
+#
+#with conn.begin():
+#    with io.open('crime.json', 'w', encoding='utf-8') as f:
+#        f.write(json.dumps([
+#            {
+#                'crime': outer.crime,
+#                'incidents': [
+#                    {
+#                        'month': inner.month.strftime('%Y-%m-%d'),
+#                        'count': inner.incidents,
+#                    }
+#                    for inner in
+#                    conn.execute(sa.select([table.c.month, table.c.incidents]).
+#                                 where(table.c.crime == outer.crime).
+#                                 order_by(table.c.month))
+#                ],
+#            }
+#            for outer in conn.execute(sa.select([table.c.crime], distinct=True).
+#                                      order_by(
+#                                          sa.func.coalesce(
+#                                              sa.func.nullif(table.c.crime, 'Total'), 'ZZZ')))
+#        ]))
